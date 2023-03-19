e33ab0c757b6e5e35bb04674aca3fcb232d34b07
moyoez <i@himoyo.cn>
2023-03-20 00:36:02 +0800

    fix

--- aua.go
+++ aua.go
@@ -6,63 +6,71 @@ import (
 )
 
 // GetUserInfo Url 为完整的 ArcaeaAPI 请求，token 为此项目需要用到的 Auth，id是需要查询使用的id。
-func GetUserInfo(url string, token string, arcaeaid string) (reply []byte, err error) {
+func GetUserInfo(url string, token string, arcaeaid string) (reply string, err error) {
 	reply, err = DrawRequestArc(url+"/botarcapi/user/info?user="+arcaeaid+"&recent=1&withsonginfo=true", token)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	return reply, err
 }
 
 // Best30 可以参考GetUserInfo
-func Best30(url string, token string, arcaeaid string) (reply []byte, err error) {
+func Best30(url string, token string, arcaeaid string) (reply string, err error) {
 	reply, err = DrawRequestArc(url+"/botarcapi/user/best30?user="+arcaeaid+"&withrecent=false&overflow=10&withsonginfo=true", token)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	return reply, err
 }
 
-func GetUserBest(url string, token string, arcaeaid string, songname string, difficuity string) (reply []byte, err error) {
+func GetUserBest(url string, token string, arcaeaid string, songname string, difficuity string) (reply string, err error) {
 	reply, err = DrawRequestArc(url+"/botarcapi/user/best?user="+arcaeaid+"&songname="+songname+"&difficulty="+difficuity+"&withsonginfo=true", token)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	return reply, err
 }
 
-func GetSongRandom(url string, token string, start string, end string) (reply []byte, err error) {
+func GetSongRandom(url string, token string, start string, end string) (reply string, err error) {
 	reply, err = DrawRequestArc(url+"/botarcapi/song/random?start="+start+"&end="+end+"&withsonginfo=true", token)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	return reply, err
 }
 
-func GetSongInfo(url string, token string, songname string) (reply []byte, err error) {
+func GetSongInfo(url string, token string, songname string) (reply string, err error) {
 	reply, err = DrawRequestArc(url+"/botarcapi/song/info?songname="+songname, token)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	return reply, err
 }
 
-func GetSongPreview(url string, token string, songname string, difficuity string) (reply []byte, err error) {
+func GetSongPreview(url string, token string, songname string, difficuity string) (reply string, err error) {
 	reply, err = DrawRequestArc(url+"/botarcapi/assets/preview?songname="+songname+"&difficulty="+difficuity, token)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	return reply, err
 }
 
-func DrawRequestArc(workurl string, token string) (reply []byte, err error) {
-	replyByte, err := http.Get(workurl)
+func DrawRequestArc(workurl string, token string) (reply string, err error) {
+	replyByte, err := http.NewRequest("GET", workurl, nil)
 	replyByte.Header.Set("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
 	replyByte.Header.Set("Authorization", "Bearer "+token)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	defer replyByte.Body.Close()
-	reply, err = io.ReadAll(replyByte.Body)
+	resp, err := http.DefaultClient.Do(replyByte)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	replyBack, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	reply = string(replyBack)
 	return reply, err
 }
